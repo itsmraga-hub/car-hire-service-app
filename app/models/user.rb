0a148373d712071ebf50b33646347146bb6d47d6
Raga <ragawilliam570@gmail.com>
2023-01-19 22:23:20 +0300

    Add has many cars

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :reservations
+  has_many :cars
+
   belongs_to :role
 
   after_initialize :set_default_role!
