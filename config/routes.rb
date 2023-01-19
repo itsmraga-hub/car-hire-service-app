fd9579eda1da57a1c4de69cf11b1d5265a300a23
Raga <ragawilliam570@gmail.com>
2023-01-19 22:23:57 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  # devise_for :users, controllers: {
+  #   sessions: 'users/sessions',
+  #   registrations: 'users/registrations',
+  # }
   resources :reservations
   resources :cars
-  resources :users
+  resources :users do
+    resources :reservations
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root 'cars#index'
 end
