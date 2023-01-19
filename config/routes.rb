45a46ba0e989130b3a1ab617ab17988b08c93e7d
Raga <ragawilliam570@gmail.com>
2023-01-19 22:26:06 +0300

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations',
   }
+  
   resources :reservations
   resources :cars
   resources :users do
